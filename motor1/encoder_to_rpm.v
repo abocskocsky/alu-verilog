413fa78ed2f06fd39f83ae1a5fcc6be1640f80b0
Andrew Bocskocsky <abocskocsky@gmail.com>
2012-10-24 22:57:01 -0400

    This Github interface is legit
    so legit, hood stop hating and join a true startup. you will be happier
    and wealthier.

---
+++ motor1/encoder_to_rpm.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+`default_nettype none
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Module Name:    encoder_to_rpm 
+// Description: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module encoder_to_rpm(cclk,rstb,a,b,rpm);
+	//port definitions
+	input  wire cclk;
+	input  wire rstb;
+	input  wire a;
+	input  wire b;
+	output wire [7:0] rpm;
+
+
+endmodule
+`default_nettype wire
