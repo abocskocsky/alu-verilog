3e2bd5a846ca98f8436cb50577811ddd1e57276a
Byron Hood <byron.hood@college.harvard.edu>
2012-11-08 22:52:01 -0500

    Finished the register file.

--- mips_stub/register_file.v
+++ mips_stub/register_file.v
@@ -18,24 +18,18 @@ module register_file(cclk,rstb,write,read_reg_0,read_reg_1,write_reg,write_data,
 	output reg [31:0] reg0;
 	output reg [31:0] reg1;
    
+   // Hold the state of the register file.
    reg [31:0][31:0] registers;
-   
-   wire [31:0][31:0] _out;
-   wire [31:0] write_reg_index, read_reg_0_index, read_reg_1_index;
-        
-   decoder_32bit WRITE_DECODER (.C(write_reg), .R(write_reg_index));
-   decoder_32bit REG0_DECODER (.C(read_reg_0), .R(read_reg_0_index));
-   decoder_32bit REG1_DECODER (.C(read_reg_1), .R(read_reg_1_index));
    
    always @(posedge cclk) begin
       if (~rstb) begin
          reg0 <= 32'b0;
          reg1 <= 32'b0;
       end else begin
-         reg0 <= registers[read_reg_0_index];
-         reg1 <= registers[read_reg_1_index];
+         reg0 <= registers[read_reg_0];
+         reg1 <= registers[read_reg_1];
          if (write) begin
-            registers[write_reg_index] <= write_data;
+            registers[write_reg] <= write_data;
          end
       end
    end
