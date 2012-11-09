d4ef72d708a65f88ca07eebb161955b96e421734
Byron Hood <byron.hood@college.harvard.edu>
2012-11-09 13:30:51 -0500

    Updated register file stuff

--- mips_stub/register_file.v
+++ mips_stub/register_file.v
@@ -19,12 +19,44 @@ module register_file(cclk,rstb,write,read_reg_0,read_reg_1,write_reg,write_data,
 	output reg [31:0] reg1;
    
    // Hold the state of the register file.
-   reg [31:0][31:0] registers;
+   reg [31:0] registers[31:0];
    
    always @(posedge cclk) begin
       if (~rstb) begin
          reg0 <= 32'b0;
          reg1 <= 32'b0;
+         registers[0] <= 32'b0;
+         registers[1] <= 32'b0;
+         registers[2] <= 32'b0;
+         registers[3] <= 32'b0;
+         registers[4] <= 32'b0;
+         registers[5] <= 32'b0;
+         registers[6] <= 32'b0;
+         registers[7] <= 32'b0;
+         registers[8] <= 32'b0;
+         registers[9] <= 32'b0;
+         registers[10] <= 32'b0;
+         registers[11] <= 32'b0;
+         registers[12] <= 32'b0;
+         registers[13] <= 32'b0;
+         registers[14] <= 32'b0;
+         registers[15] <= 32'b0;
+         registers[16] <= 32'b0;
+         registers[17] <= 32'b0;
+         registers[18] <= 32'b0;
+         registers[19] <= 32'b0;
+         registers[20] <= 32'b0;
+         registers[21] <= 32'b0;
+         registers[22] <= 32'b0;
+         registers[23] <= 32'b0;
+         registers[24] <= 32'b0;
+         registers[25] <= 32'b0;
+         registers[26] <= 32'b0;
+         registers[27] <= 32'b0;
+         registers[28] <= 32'b0;
+         registers[29] <= 32'b0;
+         registers[30] <= 32'b0;
+         registers[31] <= 32'b0;
       end else begin
          reg0 <= registers[read_reg_0];
          reg1 <= registers[read_reg_1];
