3996f17be1a79b999d2f09ace9eb138ea0ea79f3
Byron Hood <byron.hood@college.harvard.edu>
2012-11-09 14:00:32 -0500

    Removing Xilinx files

--- mips_stub/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Documents and Settings/student/My Documents/GitHub/alu-verilog/mips_stub/alu_control_test_isim_beh.exe" -prj "C:/Documents and Settings/student/My Documents/GitHub/alu-verilog/mips_stub/alu_control_test_beh.prj" "work.alu_control_test" "work.glbl" 
