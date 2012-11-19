796f99b5b12f9fe780605bca90ad82b99c0479c5
Byron Hood <byron.hood@college.harvard.edu>
2012-11-19 00:44:10 -0500

    Put truth table from notes into control unit

--- mips_stub/control_unit.v
+++ mips_stub/control_unit.v
@@ -6,19 +6,30 @@
 // Description: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module control_unit(I,RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp1,ALUOp2);
-	//port definitions
+
+`define INST_FETCH   4'b0000
+`define INST_DECODE  4'b0001
+`define INST_EXEC_M  4'b0010
+`define INST_MEM_L   4'b0011
+`define INST_WRITE   4'b0100
+`define INST_MEM_S   4'b0101
+`define INST_EXEC_R  4'b0110
+`define INST_MEM_R   4'b0111
+`define INST_EXEC_B  4'b1000
+`define INST_EXEC_J  4'b1001
+`define INST_ILLEGAL 4'b1111
+
+module control_unit(I, State, PcWriteCond, PcWrite, IorD, MemRead, MemWrite,
+   MemToReg, IrWrite, PcSource, AluOp, AluSrcA, AluSrcB, RegWrite, RegDst, NextState);
+	
+   //port definitions
 	input  wire [31:0] I;
-	output wire RegDst;
-	output wire ALUSrc;
-	output wire MemtoReg;
-	output wire RegWrite;
-	output wire MemRead;
-	output wire MemWrite;
-	output wire Branch;
-   output wire BranchNEq;
-   output wire Jump;
-	output wire [2:0] ALUOp;
+   input  wire [3:0] State;
+   output reg  PcWriteCond, PcWrite, IorD, MemRead, MemWrite,
+               MemToReg, IrWrite, AluSrcA, RegWrite, RegDst;
+   output reg  [1:0] PcSource, AluSrcB;
+   output wire [2:0] AluOp;
+   output reg  [3:0] NextState;
 
 	wire R;  // r-type
 	wire L;  // load type
@@ -30,7 +41,127 @@ module control_unit(I,RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AL
 	assign L = I[31] & ~I[30] & ~I[29] & ~I[28] & I[27] & I[26];
 	assign S = I[31] & ~I[30] & I[29] & ~I[28] & I[27] & I[26];
 	assign B = ~I[31] & ~I[30] & ~I[29] & I[28] & ~I[27];
-   assign J = ~I[31] & ~I[30] & ~I[29] & ~I[28] & I[27];
+   assign J = (~I[31] & ~I[30] & ~I[29] & ~I[28] & I[27]) | (R & (I[20:0] & 20'b1000));
+   // 1st bit: R- or I-type (excl. branch), 2nd bit: branch type, 3rd bit: load or store type
+	assign ALUOp = {(~B & ~J & ~L & ~S), B, (L | S)};
+   
+   always @(*) begin
+      case (State)
+      `INST_FETCH: begin
+         PcWrite <= 1'b1;
+         IorD <= 1'b0;
+         MemRead <= 1'b1;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b1;
+         PcSource <= 2'b00;
+         AluSrcA <= 1'b0;
+         AluSrcB <= 2'b01;
+         RegWrite <= 1'b0;
+         NextState <= `INST_DECODE;
+      end
+      INST_DECODE: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         AluSrcA <= 1'b0;
+         AluSrcB <= 2'b11;
+         RegWrite <= 1'b0;
+         if (R) NextState <= `INST_EXEC_R;
+         else if (J) NextState <= `INST_EXEC_J;
+         else if (B) NextState <= `INST_EXEC_B;
+         else if (L | S) NextState <= `INST_EXEC_M;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_EXEC_M: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         AluSrcA <= 1'b1;
+         AluSrcB <= 2'b10;
+         RegWrite <= 1'b0;
+         if (L) NextState <= `INST_MEM_L;
+         else if (S) NextState <= `INST_MEM_S;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_MEM_L: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         IorD <= 1'b1;
+         MemRead <= 1'b1;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         RegWrite <= 1'b0;
+         if (L) NextState <= `INST_WRITE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_WRITE: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         MemToReg <= 1'b1;
+         RegWrite <= 1'b1;
+         RegDst <= 1'b0;
+         if (L) NextState <= `INST_DECODE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_MEM_S: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         IorD <= 1'b1;
+         MemWrite <= 1'b1;
+         IrWrite <= 1'b0;
+         RegWrite <= 1'b0;
+         if (S) NextState <= `INST_DECODE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_EXEC_R: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         AluSrcA <= 1'b1;
+         RegWrite <= 1'b0;
+         if (R) NextState <= `INST_MEM_R;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_MEM_R: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b0;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         MemToReg <= 1'b0;
+         RegWrite <= 1'b1;
+         RegDst <= 1'b1;
+         if (R) NextState <= `INST_DECODE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_EXEC_B: begin
+         PcWrite <= 1'b0;
+         PcWriteCond <= 1'b1;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         PcSource <= 2'b01;
+         AluSrcA <= 1'b1;
+         AluSrcB <= 2'b00;
+         RegWrite <= 1'b0;
+         if (B) NextState <= `INST_DECODE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      INST_EXEC_J: begin
+         PcWrite <= 1'b1;
+         MemWrite <= 1'b0;
+         IrWrite <= 1'b0;
+         PcSource <= 2'b10;
+         RegWrite <= 1'b0;
+         if (J) NextState <= `INST_DECODE;
+         else NextState <= `INST_ILLEGAL;
+      end
+      default: NextState <= `INST_ILLEGAL;
+      endcase
+   end
 	
 	assign RegDst = R;
 	assign ALUSrc = L | S;
@@ -41,8 +172,6 @@ module control_unit(I,RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AL
 	assign Branch = B;
    assign BranchNEq = B & I[26];
    assign Jump = J | (~(& I[31:26]) & (I[20:0] & 20'b1000));
-   // 1st bit: R- or I-type, 2nd bit: branch type, 3rd bit: load or store type
-	assign ALUOp = {(~B & ~J & ~L & ~S), B, (L | S)};
 
 endmodule
 `default_nettype wire
